f99946809b8c8ad210598a463c7e49a9220f773f
guzhenhua <7070@qq.com>
2020-06-09 17:41:53 +0800

    'map遍历'

--- src/main/java/com/nuanshui/frms/test/utils/http/TestCollection.java
+++ src/main/java/com/nuanshui/frms/test/utils/http/TestCollection.java
@@ -1,7 +1,9 @@
 package com.nuanshui.frms.test.utils.http;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public class TestCollection {
     public static void main(String[] args) {
@@ -12,23 +14,31 @@ public class TestCollection {
         list.add("4");
         list.add("5");
         list.add("6");
-//1.遍历fore
-        for (String item :list) {
-            System.out.println(item);
+////1.遍历fore
+//        for (String item : list) {
+//            System.out.println(item);
+//        }
+//
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i));
+//        }
+//
+////  2.遍历迭代器
+//        Iterator<String> iterator = list.iterator();
+//        while (iterator.hasNext()) {
+//            System.out.println("------" + iterator.next());
+//        }
+////  3.增删改查  add remove get
+//        list.remove("6");
+//        System.out.printf("" + list);
+   //map遍历
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("张三","18");
+        map.put("李四","28");
+        map.put("王五","8");
+        for (String key:map.keySet()) {
+            System.out.println(key+"-----"+map.get(key));
         }
-
-        for (int i =0 ;i<list.size();i++){
-            System.out.println(list.get(i));
-        }
-
-//  2.遍历迭代器
-        Iterator<String> iterator = list.iterator();
-        while (iterator.hasNext()){
-            System.out.println("------"+iterator.next());
-        }
-//  3.增删改查  add remove get
-        list.remove("6");
-        System.out.printf(""+list);
     }
 
 }

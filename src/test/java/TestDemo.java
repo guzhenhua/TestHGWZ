00d9386382f6cb1a137c1bff4358355513e1d26c
guzhenhua <7070@qq.com>
2020-06-12 09:56:23 +0800

    '添加单元测试第一个例子'

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestDemo {
+    @Test
+    void testDemo(){
+        assertEquals(1,1);
+    }
+
+}

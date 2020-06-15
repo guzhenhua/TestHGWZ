22b5820800bef67669ff8805736b9f7a4f343a86
guzhenhua <7070@qq.com>
2020-06-15 20:27:25 +0800

    '参数化 自带NUll和空参数    @NullSource  @EmptySource'

--- src/test/java/TestPram.java
+++ src/test/java/TestPram.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.NullString;
 
 
 public class TestPram {
@@ -10,4 +14,17 @@ public class TestPram {
         Assertions.assertTrue(argus>=0);
     }
 
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings={"3"})
+    void testPramDemo1(String argus){
+        Assertions.assertTrue(argus.equals("3"));
+    }
+
+    @ParameterizedTest
+    @EnumSource
+    void testPramDemo2(int argus){
+        Assertions.assertTrue(argus>=0);
+    }
 }

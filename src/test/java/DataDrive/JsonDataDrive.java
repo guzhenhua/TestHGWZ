b8f59796a0fca1aa291c6d7c8d2d3705aae0ffc4
guzhenhua <7070@qq.com>
2020-06-16 16:59:48 +0800

    '演示案例结束'

--- src/test/java/DataDrive/JsonDataDrive.java
+++ src/test/java/DataDrive/JsonDataDrive.java
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.io.IOException;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class JsonDataDrive {

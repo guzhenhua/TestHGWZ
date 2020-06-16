dcf0a6442db10baa3fe0f0140751205020e60d79
guzhenhua <7070@qq.com>
2020-06-16 10:47:58 +0800

    '生成动态测试用例@TestFactory'

---
+++ src/test/java/DynamicTestsDemo.java
@@ -0,0 +1,91 @@
+import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.DynamicContainer.dynamicContainer;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+public class DynamicTestsDemo {
+    @TestFactory
+    List<String> dynamicTestsWithInvalidReturnType() {
+        return Arrays.asList("Hello");
+    }
+
+    @TestFactory
+    Collection<DynamicTest> dynamicTestsFromCollection() {
+        return Arrays.asList(
+                dynamicTest("1st dynamic test", () -> assertTrue("madam".equals("123"))),
+                dynamicTest("2nd dynamic test", () -> assertEquals(4, 4))
+        );
+    }
+
+    @TestFactory
+    Iterable<DynamicTest> dynamicTestsFromIterable() {
+        return Arrays.asList(
+                dynamicTest("3rd dynamic test", () -> assertTrue("madam".equals("123"))),
+                dynamicTest("4th dynamic test", () -> assertEquals(4, 4))
+        );
+    }
+
+    @TestFactory
+    Iterator<DynamicTest> dynamicTestsFromIterator() {
+        return Arrays.asList(
+                dynamicTest("5th dynamic test", () -> assertTrue("madam".equals("123"))),
+                dynamicTest("6th dynamic test", () -> assertEquals(4, 4))
+        ).iterator();
+    }
+
+    @TestFactory
+    DynamicTest[] dynamicTestsFromArray() {
+        return new DynamicTest[]{
+                dynamicTest("7th dynamic test", () -> assertTrue("madam".equals("123"))),
+                dynamicTest("8th dynamic test", () -> assertEquals(4, 4))
+        };
+    }
+
+    @TestFactory
+    Stream<DynamicTest> dynamicTestsFromStream() {
+        return Stream.of("racecar", "radar", "mom", "dad")
+                .map(text -> dynamicTest(text, () -> assertTrue(text.equals("dad"))));
+    }
+
+    @TestFactory
+    Stream<DynamicTest> dynamicTestsFromIntStream() {
+        // Generates tests for the first 10 even integers.
+        return IntStream.iterate(0, n -> n + 2).limit(10)
+                .mapToObj(n -> dynamicTest("test" + n, () -> assertTrue(n % 2 == 0)));
+    }
+
+    @TestFactory
+    Stream<DynamicNode> dynamicTestsWithContainers() {
+        return Stream.of("A", "B", "C")
+                .map(input -> dynamicContainer("Container " + input, Stream.of(
+                        dynamicTest("not null", () -> assertNotNull(input)),
+                        dynamicContainer("properties", Stream.of(
+                                dynamicTest("length > 0", () -> assertTrue(input.length() > 0)),
+                                dynamicTest("not empty", () -> assertFalse(input.isEmpty()))
+                        ))
+                )));
+    }
+
+    @TestFactory
+    DynamicNode dynamicNodeSingleTest() {
+        return dynamicTest("'pop' is a palindrome", () -> assertTrue("pop".equals("123")));
+    }
+
+    @TestFactory
+    DynamicNode dynamicNodeSingleContainer() {
+        return dynamicContainer("palindromes",
+                Stream.of("racecar", "radar", "mom", "dad")
+                        .map(text -> dynamicTest(text, () -> assertTrue(text.equals("mom")))
+                        ));
+    }
+}
